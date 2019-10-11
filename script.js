a8685cab7c5f23ad5d870295afaebd0f085b988b
eo Nicholls <eo.nicholls@gmail.com>
2019-10-11 21:59:47 +0100

    Gradient direction buttons now functioning.

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var randomButton = document.querySelector(".random-button");
 var body = document.querySelector("body");
+var gradientAngle = "90deg";
+var angleButtons = document.querySelectorAll(".angle-button");
 
 function setGradient() {
 	body.style.background = 
-		"linear-gradient(to right, " 
+		"linear-gradient("
+		+ gradientAngle
+		+ ", "
 		+ color1.value 
 		+ ", " 
 		+ color2.value 
@@ -18,6 +22,9 @@ function setGradient() {
 function setRandomGradient() {
 	color1.value = getRandomColor();
 	color2.value = getRandomColor();
+	let angle = getGradientAngleFromButtonClassList(
+		angleButtons[Math.floor(Math.random() * angleButtons.length)]);
+	setGradientAngle(angle);
 	setGradient();
 }
 
@@ -26,8 +33,25 @@ function getRandomColor() {
 	return "#000000".slice(0, -color.length) + color;
 }
 
+function setGradientAngle(angle) {
+	gradientAngle = angle;
+}
+
+function getGradientAngleFromButtonClassList(button) {
+	return button.classList[0];
+}
+
+function addEventListenerToGradientAngleButton(button) {
+	button.addEventListener("click", function() {
+		let angle = getGradientAngleFromButtonClassList(button);
+		setGradientAngle(angle);
+		setGradient();
+	});
+}
+
 setRandomGradient();
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 randomButton.addEventListener("click", setRandomGradient);
+angleButtons.forEach(addEventListenerToGradientAngleButton);
