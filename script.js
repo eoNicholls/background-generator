17026dc003b4b031eaaa6c6a577b05f1fb57a245
eo Nicholls <eo.nicholls@gmail.com>
2019-10-12 17:27:11 +0100

    further imrovements to script.js format/readability, tried to improve functional determinism

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const body = document.querySelector("body");
-const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const color1Wrapper = document.querySelector("#color1-wrapper");
 const color2Wrapper = document.querySelector("#color2-wrapper");
 const randomButton = document.querySelector(".random-button");
 const angleButtons = document.querySelectorAll(".angle-button");
+const css = document.querySelector("h3");
 
 let gradientAngle = "90deg";
 
 
-const setGradient = () => {
+const getRandomColor = () => {
+	let color = Math.floor(Math.random() * 16777216).toString(16);
+	return "#000000".slice(0, -color.length) + color;
+}
+
+const getGradientAngleFromButtonClassList = (button) => {
+	return button.classList[0];
+}
+
+const getRandomAngle = () => {
+	let angle = Math.floor(Math.random() * 360);
+	return angle + "deg";
+}
+
+
+const setRandomBackgroundValues = () => {
+	gradientAngle = getRandomAngle();
+	color1.value = getRandomColor();
+	color2.value = getRandomColor();
+}
+
+const setBackground = () => {
 	body.style.background = 
 		"linear-gradient("
 		+ gradientAngle
@@ -20,55 +41,26 @@ const setGradient = () => {
 		+ color2.value 
 		+")";
 	
-	setColorWrapperBackgrounds();
+	color1Wrapper.setAttribute("style", "background-color: " + color1.value + ";");
+	color2Wrapper.setAttribute("style", "background-color: " + color2.value + ";");
 	css.textContent = body.style.background +";";
 }
 
-const setRandomGradient = () => {
-	color1.value = getRandomColor();
-	color2.value = getRandomColor();
-}
 
-const getRandomColor = () => {
-	let color = Math.floor(Math.random() * 16777216).toString(16);
-	return "#000000".slice(0, -color.length) + color;
-}
+setRandomBackgroundValues();
+setBackground();
 
-const setGradientAngle = (angle) => {
-	gradientAngle = angle;
-}
 
-const getGradientAngleFromButtonClassList = (button) => {
-	return button.classList[0];
-}
-
-const addEventListenerToGradientAngleButton = (button) => {
+const addEventListenerToAngleButton = (button) => {
 	button.addEventListener("click", function() {
 		let angle = getGradientAngleFromButtonClassList(button);
-		setGradientAngle(angle);
-		setGradient();
+		gradientAngle = angle;
+		setBackground();
 	});
 }
 
-const setRandomAngle = () => {
-	let angle = Math.floor(Math.random() * 360);
-	gradientAngle = angle + "deg";
-}
-
-const setRandomAngleAndGradient = () => {
-	setRandomAngle();
-	setRandomGradient();
-	setGradient();
-}
-
-const setColorWrapperBackgrounds = () => {
-	color1Wrapper.setAttribute("style", "background-color: " + color1.value + ";");
-	color2Wrapper.setAttribute("style", "background-color: " + color2.value + ";");
-}
-
-setRandomAngleAndGradient();
-
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
-angleButtons.forEach(addEventListenerToGradientAngleButton);
-randomButton.addEventListener("click", setRandomAngleAndGradient);
+color1.addEventListener("input", setBackground);
+color2.addEventListener("input", setBackground);
+angleButtons.forEach(addEventListenerToAngleButton);
+randomButton.addEventListener("click", setRandomBackgroundValues);
+randomButton.addEventListener("click", setBackground);
