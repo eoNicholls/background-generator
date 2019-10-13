e2fe670571492d74ff70c914dfc972d519408ed6
eo Nicholls <eo.nicholls@gmail.com>
2019-10-13 22:31:08 +0100

    Lock buttons now functioning, although I need to sort out the opacity. Horizontal sliders fully functioning, but need styling.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const color1Wrapper = document.querySelector("#color1-wrapper");
 const color2Wrapper = document.querySelector("#color2-wrapper");
+const color1LockButton = document.querySelector("#color1-lock-button");
+const color2LockButton = document.querySelector("#color2-lock-button");
 const randomButton = document.querySelector(".random-button");
 const angleButtons = document.querySelectorAll(".angle-button");
 const css = document.querySelector("h3");
@@ -63,16 +65,23 @@ const copyCSSTextToClipboard = () => {
 const setRandomBackgroundValues = () => {
 	gradientAngle = getRandomAngle();
 	angleSlider.value = gradientAngle;
-	color1.value = getRandomColor();
-	color2.value = getRandomColor();
+
+	if (color1LockButton.locked != true) {
+		color1.value = getRandomColor();
+	}
+	if (color2LockButton.locked != true) {
+		color2.value = getRandomColor();
+	}
 }
 
 const setBackground = () => {
 	let c1 = `${color1.value}${gradientOpacity}`;
 	let c2 = `${color2.value}${gradientOpacity}`;
-	body.setAttribute("style", `background: linear-gradient(${gradientAngle}deg, ${c1}, ${c2});`)
+	body.setAttribute("style", `background: linear-gradient(${gradientAngle}deg, ${c1}, ${c2});`);
 	color1Wrapper.setAttribute("style", "background-color: " + color1.value + ";");
 	color2Wrapper.setAttribute("style", "background-color: " + color2.value + ";");
+	color1LockButton.setAttribute("style", "background-color: " + color1.value + ";");
+	color2LockButton.setAttribute("style", "background-color: " + color2.value + ";");
 	css.textContent = body.style.background +";";
 	angleSliderLabel.innerHTML = gradientAngle;
 	opacitySliderLabel.innerHTML = Math.round((parseInt(gradientOpacity, 16) / 255) * 100);
@@ -98,3 +107,23 @@ opacitySlider.oninput = function() {
 	gradientOpacity = Number(this.value).toString(16);
 	setBackground();
 }
+
+
+const lockInteraction = (lockButton) => {
+	if (lockButton.locked === true) {
+		lockButton.locked = false;
+		lockButton.classList.remove("fa-lock");
+		lockButton.classList.add("fa-lock-open");
+	} else {
+		lockButton.locked = true;
+		lockButton.classList.remove("fa-lock-open");
+		lockButton.classList.add("fa-lock");
+	}
+}
+
+color1LockButton.addEventListener("click", function() {
+	lockInteraction(this)
+});
+color2LockButton.addEventListener("click", function() {
+	lockInteraction(this)
+});
